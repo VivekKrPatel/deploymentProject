b5a93f2909ccdc8c05db04a6ea10dd704648dfab
Vivek_Kumar_Patel <vivekkrpatel2442@gmail.com>
2023-05-23 16:29:49 +0530

    improved issues

--- src/main/java/com/example/springBootDeploy/models/Student.java
+++ src/main/java/com/example/springBootDeploy/models/Student.java
@@ -1,6 +1,9 @@
 package com.example.springBootDeploy.models;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.Email;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -12,6 +15,8 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class Student {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private Long rollNo;
